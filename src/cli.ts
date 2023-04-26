b080cc9f699cffb487aa9beee5784ad6cfc6e5c2
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-04-26 14:42:09 +0100

    Refactor to improve coverage

--- src/cli.ts
+++ src/cli.ts
@@ -2,32 +2,20 @@
 import getConfig from "./config.js";
 import BuildInfo from "./index.js";
 import { ConsoleSink, FileSink, type Sink } from "./sinks.js";
-import { CircleCI, Clock, Git, GitHubActions, Heroku, Netlify, User } from "./sources/index.js";
 
 const enum ExitCode {
 	OK = 0,
 	ERROR = 1,
 }
 
-const buildInfo: BuildInfo = new BuildInfo([
-	new Clock(),
-	new User(),
-	new Git(),
-	new Heroku(),
-	new GitHubActions(),
-	new CircleCI(),
-	new Netlify(),
-]);
 
-const [, , ...args] = process.argv;
-
-const { output } = getConfig(args);
+const { output } = getConfig(process.argv.slice(2));
 
 const sink: Sink = output ? new FileSink(output) : new ConsoleSink();
 
 (async (): Promise<void> => {
 	try {
-		const lines = await buildInfo.lines();
+		const lines = await new BuildInfo().lines();
 		await sink.write(lines.join("\n"));
 		process.exit(ExitCode.OK);
 	} catch (err) {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
+import { CircleCI, Clock, Git, GitHubActions, Heroku, Netlify, User } from "./sources/index.js";
+
 export interface Source {
 	applies?(): boolean | Promise<boolean>;
 	lines(): string[] | Promise<string[]>;
 }
 
+const ALL_SOURCES: Source[] = [
+	new Clock(),
+	new User(),
+	new Git(),
+	new Heroku(),
+	new GitHubActions(),
+	new CircleCI(),
+	new Netlify(),
+];
+
 export default class BuildInfo implements Source {
 
-	constructor(private sources: Source[]) {}
+	constructor(private sources: Source[] = ALL_SOURCES) {}
 
 	async lines(): Promise<string[]> {
 		const lines = [];
